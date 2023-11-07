4730e361ed45b53bc355ff44ed26b71983ec328e
Jibrin Masud <jibrinmasud494@gmail.com>
2023-11-08 00:43:45 +0100

    Adding completed for a todo item

--- TodoApp/src/components/TodoItem.jsx
+++ TodoApp/src/components/TodoItem.jsx
@@ -3,10 +3,20 @@ export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete(item) {
     setTodos(todos.filter((todo) => todo !== item));
   }
+  function handleClick(name) {
+    const newArray = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newArray);
+  }
+  const className = item.done ? styles.completed : "";
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item.name}
+        <span className={className} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
+
         <span>
           <button
             onClick={() => handleDelete(item)}
